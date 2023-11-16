f6c16e34223ec87151a5bce5f9c30e54bf10ff85
OlegPlugaru <olegplugaru.md@gmail.com>
2023-11-16 21:39:28 +0200

    finished filemanager bootcamp,

---
+++ SwiftulThinkingContinuedLearning/CacheBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  CacheBootcamp.swift
+//  SwiftulThinkingContinuedLearning
+//
+//  Created by Oleg Plugaru on 16.11.2023.
+//
+
+import SwiftUI
+
+struct CacheBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CacheBootcamp()
+}
